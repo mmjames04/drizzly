22fd21004d6a5c1a4586098f571db706f447e13a
alexrivadeneira <alexrivs@gmail.com>
2015-01-22 16:32:59 -0500

    default location working on signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,23 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :sign_up_params, :account_update_params, if: :devise_controller?
+  # before_action :sign_up_params, :account_update_params, if: :devise_controller?
       
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :default_location
+  end
 
 
-		def sign_up_params
-		    params.require(:user).permit(:default_location, :email, :password, :password_confirmation)
-		  end
-		 
-		  def account_update_params
-		    params.require(:user).permit(:default_location, :email, :password, :password_confirmation, :current_password)
-		  end
+			# def sign_up_params
+			#     params.require(:user).permit(:default_location, :email, :password, :password_confirmation)
+			#   end
+			 
+			#   def account_update_params
+			#     params.require(:user).permit(:default_location, :email, :password, :password_confirmation, :current_password)
+			#   end
 
 end
