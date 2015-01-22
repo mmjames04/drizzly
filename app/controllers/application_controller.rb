7de00e756b19f19f1b50f85879b8d989616eb6c8
Andy <andrew.sunglae.kim@gmail.com>
2015-01-22 17:52:53 -0500

    change locations to cit and states, update seed file for new data type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,8 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :default_location
+    devise_parameter_sanitizer.for(:sign_up) << :city
+    devise_parameter_sanitizer.for(:sign_up) << :state
   end
 
 
---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,23 @@
+class FavoritesController < ApplicationController
+
+
+	def create
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.create(comment_params)
+		redirect_to article_path(@article)
+	end
+
+	# destroys comment and redicrect to articles page
+	def destroy
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.find(params[:id])
+		@comment.destroy
+		redirect_to article_path(@article)
+	end
+
+	private 
+
+		def comment_params
+			params.require(:comment).permit(:commenter, :body)
+		end
+end
