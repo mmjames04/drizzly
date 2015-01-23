ecf44b6f6ae3808953ebb79c4dd16412e01c934f
Andy <andrew.sunglae.kim@gmail.com>
2015-01-23 13:26:00 -0500

    framework for JS

--- app/assets/javascripts/changeweather.js
+++ app/assets/javascripts/changeweather.js
@@ -1,4 +1,15 @@
-$("select").on("change", function(){
-	var city = "city"
-	var location = "location"
+$("select").on("change", function(e){
+	e.preventDefault();
+	var city = $(":selected").val().split(",")[0]
+	var state = $(":selected").val().split(",")[1]
+	console.log(city);
+	console.log(state);
+})
+
+$("#weather_search").on("submit", function(e){
+	e.preventDefault();
+	var city = $("#weather_search_city").val();
+	var state = $("#weather_search_state").val()
+	console.log(city)
+	console.log(state)
 })
